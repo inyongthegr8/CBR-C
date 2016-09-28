59c433f88e497016ac781323f59bf024a553ffbb
inyongthegr8 <inyong42@gmail.com>
2016-09-29 03:32:29 +0800

    Partial conversion implement
    Change datatype variable = value; to datatype variable; variable =
    value; and other cases. PARTIAL

--- src/com/cbrc/ast/preprocessor/CASTPreprocessor.java
+++ src/com/cbrc/ast/preprocessor/CASTPreprocessor.java
@@ -150,6 +150,7 @@ public class CASTPreprocessor {
 									{
 										codeLine = m.replaceAll(cvals.get(i)); // replace constant variables with the actual constant values
 									}
+									
 								}
 								/* Old Solution. Commented out, deprecated.
 								if(!codeLine.startsWith("#define"))
@@ -163,9 +164,123 @@ public class CASTPreprocessor {
 							else
 							{
 								if(sc.hasNext())
+								{
+									String datatype = "";
+									//single variable declaration for multiple lines (Capture datatypes first and maybe try to capture the FIRST variable as well)
+									Pattern declarationConversion = Pattern.compile("\\b(?:(?:long\\s*|char\\s*|int\\s*|float\\s*|double\\s*)+)" // get data type
+											+ "(?:\\s+\\s*)"
+											+ "(([a-zA-Z_][a-zA-Z0-9_]*)" // get the variables
+											+ "\\s*[;,=]\\s*"
+											+ "(([a-zA-Z_][a-zA-Z0-9_]*)|" // get variable as value
+											+ "'(.|\\\\n|\\\\t|\\\\r)'|" // get char
+											+ "\\d*l|" // get long value
+											+ "\\d*|" // get integer value
+											+ "\\d*\\.\\d*f|" // get float value
+											+ "\\d*\\.\\d*d|" // get double value
+											+ "\\d*\\.\\d)" // get decimal value 
+											+ "(;|,)" // either comma or separator
+											+ "\\s*)");
+									Matcher matcha = declarationConversion.matcher(codeLine);
+									ArrayList<Integer> starts = new ArrayList<Integer>();
+									ArrayList<Integer> ends = new ArrayList<Integer>();
+									// populate all the starts and ends
+									while (matcha.find())
+									{
+										starts.add(matcha.start());
+										ends.add(matcha.end());
+									}
+									matcha.reset(); 
+									if (matcha.find())
+									{
+										String found = matcha.group();
+										// DataTypeChecker
+										if(found.startsWith("int"))
+										{
+											datatype = "int";
+										}
+										else if(found.startsWith("long"))
+										{
+											datatype = "long";
+										}
+										else if(found.startsWith("float"))
+										{
+											datatype = "float";
+										}
+										else if(found.startsWith("double"))
+										{
+											datatype = "double";
+										}
+										else if(found.startsWith("char"))
+										{
+											datatype = "char";
+										}
+										//multiple variable declaration for multiple lines (Capture datatypes first and maybe try to capture the FIRST variable as well)
+										Pattern declarationConversion2 = Pattern.compile("\\b(?!for\\s*\\(|if\\s*\\(|else\\s*\\{|do\\s*\\{|while\\s*\\(|" // loops and conditional variables prefixes
+												+ "switch\\s*\\(|default\\s*:|case\\s+(([a-zA-Z_][a-zA-Z0-9_]*)|'(.|\\\\n|\\\\t|\\\\r)')|" // switch cases prefixes
+												+ "int\\s+|long\\s+|double\\s+|float\\s+|char\\s+|printf\\s*\\(|scanf\\s*\\(|getch\\s*\\(|getchar\\s*\\()" // datatype prefixes and printf/scanf/getch/getchar prefixes
+												+ "(([a-zA-Z_][a-zA-Z0-9_]*)[,;]|" // get the uninitialised variables;
+												+ "([a-zA-Z_][a-zA-Z0-9_]*)" // get the variables with initialised variables
+												+ "(\\s*=\\s*" // equals
+												+ "(([a-zA-Z_][a-zA-Z0-9_]*)|" // get variable as value
+												+ "'(.|\\\\n|\\\\t|\\\\r)'|" // get char
+												+ "\\d*\\.\\d*f|" // get float value
+												+ "\\d*\\.\\d*d|" // get double value
+												+ "\\d*l|" // get long value
+												+ "\\d*\\.\\d*|" // get decimal value
+												+ "\\d*)))");  // get integer value
+										Matcher matcha2 = declarationConversion2.matcher(codeLine);
+										// populate from the first start to the second start
+										for(int i = 0; i < starts.size() - 1; i++)
+										{
+											matcha2.region(starts.get(i), starts.get(i+1));
+										}
+											
+										while(matcha2.find())
+										{
+											String declaration = matcha2.group();
+											// get the vars
+											Pattern equalChecker = Pattern.compile("\\b\\s*=\\s*");
+											Matcher matcha3 = equalChecker.matcher(declaration);
+											if(matcha3.find())
+											{
+												String variable = declaration.substring(0, declaration.indexOf("=")).trim();
+												String value = declaration.substring(declaration.indexOf("=") + 1, declaration.length()).trim();
+												if(value.startsWith("\'"))
+												{
+													datatype = "char";
+													if(value.equals("\'\'"))
+													{
+														value = "\' \'"; // whitespace issue with \u0020
+													}
+												}
+												else if(value.indexOf(".") >= 0)
+												{
+													int entry = 0;
+													if(codeLine.indexOf("float") >= entry)
+													{
+														datatype = "float";
+													}
+													else
+													{
+														datatype = "double";
+													}
+												}
+												System.out.println(datatype + " " + variable + ";");
+												System.out.println(variable + " = " + value + ";");
+											}
+											else
+											{
+												// not found, variable declaration only
+												System.out.println(datatype + " " + declaration);
+											}
+										}
+									}
 									writer.println(codeLine);
+								}
 								else
+								{
 									writer.print(codeLine); // EOF!
+								}
 							}
 						}
 						sc.close();
