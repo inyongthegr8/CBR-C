574e542611863d8846e7179ca730cecc5b259365
inyongthegr8 <inyong42@gmail.com>
2016-10-10 16:58:50 +0800

    Added PostDeclarationException

---
+++ src/com/cbrc/ast/preprocessor/PostDeclarationException.java
@@ -0,0 +1,15 @@
+package com.cbrc.ast.preprocessor;
+
+public class PostDeclarationException extends Exception {
+
+	public PostDeclarationException()
+	{
+		super();
+	}
+	
+	public PostDeclarationException(String message)
+	{
+		super(message);
+	}
+
+}
